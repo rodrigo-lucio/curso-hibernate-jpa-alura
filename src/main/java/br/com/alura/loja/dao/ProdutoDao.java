c8a385d3d023477babd04b54daf8e18554a9b42b
Rodrigo Lúcio <luciodigo@gmail.com>
2021-08-25 23:10:49 -0300

    Criado filtros na JPQL e filtrar por apenas uma coluna

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,17 +18,37 @@ public class ProdutoDao {
 	public void cadastrar(Produto entity) {
 		this.entityManager.persist(entity);
 	}
-	
+
 	public void remover(Produto entity) {
 		this.entityManager.remove(entity);
 	}
-	
+
 	public Produto buscarPorId(Long id) {
 		return entityManager.find(Produto.class, id);
 	}
-	
-	public List<Produto> buscarTodos(){
+
+	public List<Produto> buscarTodos() {
 		String jpql = "SELECT p from Produto p";
 		return entityManager.createQuery(jpql).getResultList();
 	}
-}	
+
+	public List<Produto> buscarPorNome(String nome) {
+		String jpql = "SELECT p from Produto p where p.nome = :nome";
+		return entityManager.createQuery(jpql)
+							.setParameter("nome", nome)
+							.getResultList();
+	}
+	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+		String jpql = "SELECT p from Produto p where p.categoria.nome = :nome"; //ja faz o join automatico com o p.categoria.nome
+		return entityManager.createQuery(jpql)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public BigDecimal buscarPrecoPorNome(String nome) {
+		String jpql = "SELECT p.preco from Produto p where p.nome = :nome";
+		return (BigDecimal) entityManager.createQuery(jpql)
+				.setParameter("nome", nome)
+				.getSingleResult();
+	}
+}
--- src/main/java/br/com/alura/loja/entity/testes/CadastroProduto.java
+++ src/main/java/br/com/alura/loja/entity/testes/CadastroProduto.java
@@ -16,17 +16,29 @@ public class CadastroProduto {
 	public static void main(String[] args) {
 
 		manipulacoesProduto(false);
-				
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
-		
+
 		Produto p = produtoDao.buscarPorId(9l);
-		System.out.println(p.toString());
-		
+		System.out.println(p);
+
+		System.out.println("buscarTodos");
 		List<Produto> produtos = produtoDao.buscarTodos();
-		
 		produtos.forEach(produto -> System.out.println(produto));
 		
+		System.out.println("buscarPorNome");
+		produtos = produtoDao.buscarPorNome("Alterei o xiaomi");
+		produtos.forEach(produto -> System.out.println(produto));
+
+		System.out.println("buscarPorNomeDaCategoria");
+		produtos = produtoDao.buscarPorNomeDaCategoria("Caixa de Som");
+		produtos.forEach(produto -> System.out.println(produto));
+		
+		
+		BigDecimal precoProduto = produtoDao.buscarPrecoPorNome("Alterei o xiaomi");
+		System.out.println(precoProduto);
+
 	}
 
 	protected static void manipulacoesProduto(boolean limparDados) {
@@ -40,10 +52,11 @@ public class CadastroProduto {
 		caixaDeSom.setDescricao("Som bem alto");
 		caixaDeSom.setPreco(new BigDecimal("450"));
 
-		
-		
 		Categoria categoria = new Categoria("Celulares");
 		celular.setCategoria(categoria);
+
+		Categoria categoria2 = new Categoria("Caixa de Som");
+		caixaDeSom.setCategoria(categoria2);
 
 		EntityManager em = JPAUtil.getEntityManager();
 
@@ -52,40 +65,46 @@ public class CadastroProduto {
 
 		em.getTransaction().begin();
 		categoriaDao.cadastrar(categoria);
+		categoriaDao.cadastrar(categoria2);
 		produtoDao.cadastrar(celular);
 		produtoDao.cadastrar(caixaDeSom);
-		
-		celular.setNome("Alterei o xiaomi");    		//Como minha entidade está persistida, esta MANAGED, gerenciada pela JPA. nesse caso vai executar um update no nome 
-		
-		em.flush();										// Flush, diferente do commit, ele apenas sincroniza mas não salva
-		em.clear();   				 					// Limpa todas as entidades gerenciadas
-		celular.setNome("Alterei o xiaomi de novo");    // Não vai executar update, pois nao esta mais gerenciada, dei o clear acima
-		
+
+		celular.setNome("Alterei o xiaomi"); // Como minha entidade está persistida, esta MANAGED, gerenciada pela JPA.
+												// nesse caso vai executar um update no nome
+
+		em.flush(); // Flush, diferente do commit, ele apenas sincroniza mas não salva
+		em.clear(); // Limpa todas as entidades gerenciadas
+		celular.setNome("Alterei o xiaomi de novo"); // Não vai executar update, pois nao esta mais gerenciada, dei o
+														// clear acima
+
 		// Ciclo de vida da entidade
 		// Quando da um NEW no objeto -> Transient
 		// Quando da um PERSIST -> Managed (Gerenciado)
 		// Commit ou Flush - Salva as alteraçoes no BD
 		// Close ou Clear - Detached - Não esta mais gerenciado
-		
-		if(limparDados) {
+
+		if (limparDados) {
 			// Agora no UPDATE
-			// Caso queira voltar a entidade para o estado Managed, eu chamo o merge e atribuo novamente para a entidade o valor
-			// Agora se fizer o setnome vai funcionar, independente se chamar antes ou depois do merge
+			// Caso queira voltar a entidade para o estado Managed, eu chamo o merge e
+			// atribuo novamente para a entidade o valor
+			// Agora se fizer o setnome vai funcionar, independente se chamar antes ou
+			// depois do merge
 
 			celular = em.merge(celular);
-			celular.setNome("Alterei o xiaomi de novo com o merge"); 
-			
+			celular.setNome("Alterei o xiaomi de novo com o merge");
+
 			em.remove(celular);
 			categoria = em.merge(categoria);
-			em.remove(categoria);				// O delete segue a mesma ideia do update, só é removido se estiver managed
-												// Depois que é feito o close ou clear, 			
-												// as entidades MANAGED passam a ser Detached, ou seja, o JPA nao esta mais gerenciando, 
-												// Diferença é que o close fecha a conexao, e o clear apenas limpa as entidades gerenciadas		
+			em.remove(categoria); // O delete segue a mesma ideia do update, só é removido se estiver managed
+									// Depois que é feito o close ou clear,
+									// as entidades MANAGED passam a ser Detached, ou seja, o JPA nao esta mais
+									// gerenciando,
+									// Diferença é que o close fecha a conexao, e o clear apenas limpa as entidades
+									// gerenciadas
 		}
-		
+
 		em.getTransaction().commit();
-		em.close();	
+		em.close();
 	}
 
-	
 }
